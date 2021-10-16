198388c4d4005c91b02a5a73e327b90f4092eda2
saadoundhirat <saadoundhirat93@gmail.com>
2021-10-16 03:31:51 +0300

    docs: organizing files

---
+++ codechallenge/javascript/arraypractice.js
@@ -0,0 +1,7 @@
+// solving some array problems from leet codes and other sources
+
+// array Two sum
+// => given an array of numbers and target find the numbers some of the target 
+const twoSum = (arr)=>{
+    
+}
